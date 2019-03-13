a59a30f16d528db7401be14ae5588fa7126caf17
choipak <cai1057701783@gmail.com>
2019-03-13 20:59:10 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'rrg',
+      home: new Container(
+        color: Colors.blue,
+        child: SafeArea(
+          child: SizedBox.expand(
+            child: Card(color: Colors.yellowAccent),
+          ),
+        ),
+      ),
+      theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
