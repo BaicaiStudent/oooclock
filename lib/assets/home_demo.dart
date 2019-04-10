f840bf254da9bc0b1559e467bcf882ec7a9936eb
choipak <cai1057701783@gmail.com>
2019-04-10 11:41:22 +0800

    4-10

--- lib/assets/home_demo.dart
+++ lib/assets/home_demo.dart
@@ -9,35 +9,36 @@ class HomeDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'search',
-              onPressed: () => debugPrint('search-pr'),
-            ),
-            title: Text('oooclock'),
-            centerTitle: true,
-            actions: <Widget>[
-            ],
-            elevation: 0.0,
-            bottom: TabBar(
-              unselectedLabelColor: Colors.black38,
-              indicatorColor: Colors.yellowAccent,
-              indicatorSize: TabBarIndicatorSize.label,
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.calendar_today),
-                ),
-                Tab(
-                  icon: Icon(Icons.done),
-                ),
-                Tab(
-                  icon: Icon(Icons.done_all),
-                )
-              ],
-            ),
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'search',
+            onPressed: () => debugPrint('search-pr'),
           ),
+          title: Text('oooclock'),
+          centerTitle: true,
+          actions: <Widget>[],
+          elevation: 0.0,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.yellowAccent,
+            indicatorSize: TabBarIndicatorSize.label,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.calendar_today),
+              ),
+              Tab(
+                icon: Icon(Icons.done),
+              ),
+              Tab(
+                icon: Icon(Icons.done_all),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
