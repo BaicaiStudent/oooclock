9f3ae45c65250c875693f92ff68309e87bb86cee
choipak <cai1057701783@gmail.com>
2019-04-15 22:44:34 +0800

    todo-add

--- lib/assets/todo_demo.dart
+++ lib/assets/todo_demo.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart'; //待办事项
 
+void main() => runApp(new ToDo());
+
 class ToDo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[100],
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Icon(
-              Icons.format_list_numbered,
-              size: 32.0,
-              color: Colors.white,
-            ),
-            padding: EdgeInsets.all(10.0),
-            width: 60.0,
-            height: 60.0,
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(30.0)),
-          )
-        ],
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ToDoDemo());
+  }
+}
+
+class ToDoDemo extends StatefulWidget {
+  @override
+  _ToDoDemoState createState() => _ToDoDemoState();
+}
+
+class _ToDoDemoState extends State<ToDoDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Todo List',
+      home: new Scaffold(
+        // appBar: new AppBar(title: new Text('Todo List')),
+        body: new Container(),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () => debugPrint('Add-pr'),
+          tooltip: 'Add task',
+          child: new Icon(
+            Icons.add_circle,
+            color: Colors.yellow,
+          ),
+          // elevation: 3.0,
+          // highlightElevation: 2.0,
+          backgroundColor: Colors.blue, // 红色
+        ),
       ),
     );
   }
